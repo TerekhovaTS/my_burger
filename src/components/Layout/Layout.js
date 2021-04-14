a6142f21ab17fd34f7b53e3c8341681174d582f1
TerekhovaTS <romashka.tts@gmail.com>
2021-04-14 23:02:26 +0300

    First commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import Auxiliary from "../../hoc/Auxiliary";
+import styles from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+
+const Layout = (props) => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+
+  const sideDrawerCloseHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
+  };
+
+  return (
+    <Auxiliary>
+      <Toolbar
+        isAuth={props.isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler}
+      />
+      <SideDrawer
+        isAuth={props.isAuthenticated}
+        open={showSideDrawer}
+        closed={sideDrawerCloseHandler}
+      />
+      <main className={styles.Content}>{props.children}</main>
+    </Auxiliary>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
---
+++ src/components/Order/Order.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styles from "./Order.module.css";
+
+const order = (props) => {
+  const ingredients = [];
+  for (let ingredientsName in props.ingredients) {
+    ingredients.push({
+      name: ingredientsName,
+      amount: props.ingredients[ingredientsName],
+    });
+  }
+  const ingredientsOutput = ingredients.map((ig) => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px",
+        }}
+        key={ig.name}
+      >
+        {ig.name}({ig.amount})
+      </span>
+    );
+  });
+  return (
+    <div className={styles.Order}>
+      <p>Ingredients: {ingredientsOutput}</p>
+      <p>
+        Price: <strong>USD {props.price}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default order;
